9d1aaa222d6a7dd51e833d8308e3035e6215e399
Akhilesh2002 <akhileshmehra80@gmail.com>
2021-02-10 12:45:40 +0530

    Updated Files

---
+++ Assets/Scripts/TapToPlace.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class TapToPlace : MonoBehaviour
+{
+    private ARRaycastManager raycastManager;
+    private GameObject spawnObject;
+    [SerializeField]
+    private GameObject objectToPlace;
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount>0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+    private void Update()
+    {
+        if(!TryGetTouchPosition(out Vector2 touchPosition))
+        {
+            return;
+        }
+        if(raycastManager.Raycast(touchPosition,s_Hits,TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = s_Hits[0].pose;
+            if(spawnObject == null)
+            {
+                spawnObject = Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
+
+            }
+            else
+            {
+                spawnObject.transform.position = hitPose.position;
+                spawnObject.transform.rotation = hitPose.rotation;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TextBubble.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBubble : MonoBehaviour
+{
+    public GameObject textBoxUI;
+    public Text textDisplay;
+    public InputField inputField;
+    private string inputString;
+    public void OnKeyBoardLogoPressed()
+    {
+        textBoxUI.SetActive(true);
+    }
+    public void OnCancelButtonClicked()
+    {
+        textBoxUI.SetActive(false);
+    }
+    public void OnOkButtonClicked()
+    {
+        inputString = inputField.text;
+        textBoxUI.SetActive(false);
+        Debug.Log(inputString);
+    }
+}
